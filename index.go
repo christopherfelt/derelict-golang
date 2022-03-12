83e32aa5f28df5c57f1cc32548576d9ca305e166
christopherfelt <chris.h.felt@gmail.com>
2022-03-12 09:34:17 -0700

    init

---
+++ index.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Power levels criticially low...")
+	fmt.Println("")
+}
+
+func help() {
+
+}
---
+++ test.go
@@ -0,0 +1,7 @@
+package index
+
+import "fmt"
+
+func index() {
+	fmt.Println("hello world this is test")
+}
